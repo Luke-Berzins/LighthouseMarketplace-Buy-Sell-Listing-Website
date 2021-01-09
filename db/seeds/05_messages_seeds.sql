92e5fc86af4bed67285146eb967014486c8a3595
Robert <rbbenett0614@gmail.com>
2021-01-09 01:57:31 +0000

    Conversations comes before messages

--- db/seeds/05_messages_seeds.sql
+++ db/seeds/05_messages_seeds.sql
@@ -22,11 +22,11 @@ INSERT INTO messages (sender_id, receiver_id, content, conversation_id, time_sen
 INSERT INTO messages (sender_id, receiver_id, content, conversation_id, time_sent) VALUES ( 9, 3, 'you selling that?', 5, '2009-12-31T08:16:05.000Z');
 INSERT INTO messages (sender_id, receiver_id, content, conversation_id, time_sent) VALUES ( 2, 6, 'you selling that?', 6, '2009-12-31T08:09:00.000Z');
 INSERT INTO messages (sender_id, receiver_id, content, conversation_id, time_sent) VALUES ( 3, 4, 'you selling that?', 7, '2009-12-31T08:16:00.000Z');
-INSERT INTO messages (sender_id, receiver_id, content, conversation_id, time_sent) VALUES ( 3, 5, 'you selling that?', 8, '2018-12-321T08:16:00.000Z');
+INSERT INTO messages (sender_id, receiver_id, content, conversation_id, time_sent) VALUES ( 3, 5, 'you selling that?', 8, '2018-12-31T08:16:00.000Z');
 INSERT INTO messages (sender_id, receiver_id, content, conversation_id, time_sent) VALUES ( 1, 2, 'you selling that?', 1, '2010-12-31T08:16:00.000Z');
 INSERT INTO messages (sender_id, receiver_id, content, conversation_id, time_sent) VALUES ( 1, 10, 'you selling that?', 1, '2018-12-31T08:14:00.000Z');
 INSERT INTO messages (sender_id, receiver_id, content, conversation_id, time_sent) VALUES ( 4, 2, 'its been a while, hmu on myspace', 2, '2016-12-31T08:16:00.000Z');
 INSERT INTO messages (sender_id, receiver_id, content, conversation_id, time_sent) VALUES ( 5, 9, 'its been a while, hmu on myspace', 4, '2018-12-30T08:16:00.000Z');
-INSERT INTO messages (sender_id, receiver_id, content, conversation_id, time_sent) VALUES ( 3, 9, 'its been a while, hmu on myspace', 5, '2018-11-31T08:16:00.000Z');
+INSERT INTO messages (sender_id, receiver_id, content, conversation_id, time_sent) VALUES ( 3, 9, 'its been a while, hmu on myspace', 5, '2018-11-23T08:16:00.000Z');
 INSERT INTO messages (sender_id, receiver_id, content, conversation_id, time_sent) VALUES ( 6, 2, 'its been a while, hmu on myspace', 6, '2018-12-31T08:11:00.000Z');
 
