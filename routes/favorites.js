e786611161b17c1d74bb2e24f1e1d8db21f12b1a
Robert <rbbenett0614@gmail.com>
2021-01-09 17:22:38 +0000

    Added api routes for messages, favorites, and conversations

--- routes/favorites.js
+++ routes/favorites.js
@@ -10,10 +10,10 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
+    db.query(`SELECT * FROM favorites;`)
       .then(data => {
-        const users = data.rows;
-        res.json({ users });
+        const favorites = data.rows;
+        res.json({ favorites });
       })
       .catch(err => {
         res
--- routes/messages.js
+++ routes/messages.js
@@ -10,10 +10,10 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
+    db.query(`SELECT * FROM messages;`)
       .then(data => {
-        const users = data.rows;
-        res.json({ users });
+        const messages = data.rows;
+        res.json({ messages });
       })
       .catch(err => {
         res
