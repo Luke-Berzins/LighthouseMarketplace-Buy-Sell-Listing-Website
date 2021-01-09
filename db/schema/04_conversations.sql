d9506a40154fe15a7a657baedaa8fca17612acb4
saadkureshi <saq619@gmail.com>
2021-01-09 02:08:57 +0000

    all schemas updated

--- db/schema/04_conversations.sql
+++
@@ -1,9 +0,0 @@
--- Drop and recreate conversations table
-
-DELETE TABLE IF EXISTS conversations CASCADE;
-CREATE TABLE conversations (
-  id SERIAL PRIMARY KEY NOT NULL,
-  seller_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  buyer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  posting_id INTEGER REFERENCES postings(id) ON DELETE CASCADE
-);
