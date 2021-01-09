187ce095c8d18204666bbbc24a37cd0317bc6f2a
Robert <rbbenett0614@gmail.com>
2021-01-09 17:06:46 +0000

    Add api request for postings, Granted priovelages for user in psql

---
+++ routes/messages.js
@@ -0,0 +1,25 @@
+/*
+ * All routes for Messages are defined here
+ * Since this file is loaded in server.js into api/messages,
+ *   these routes are mounted onto /messages
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM users;`)
+      .then(data => {
+        const users = data.rows;
+        res.json({ users });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
