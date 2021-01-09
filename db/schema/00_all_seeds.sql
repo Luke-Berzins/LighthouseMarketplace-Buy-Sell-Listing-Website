cd9da9e56548fb3b927c9237909e1544384d0c67
Robert <rbbenett0614@gmail.com>
2021-01-09 01:30:29 +0000

    Added Databases Tables to single file

---
+++ db/schema/00_all_seeds.sql
@@ -0,0 +1,59 @@
+-- Drop and recreate users table
+
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  is_admin BOOLEAN DEFAULT FALSE
+);
+
+-- Drop and recreate postings table
+
+DROP TABLE IF EXISTS postings CASCADE;
+CREATE TABLE postings (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  image_photo_url TEXT,
+  title VARCHAR(255) NOT NULL,
+  price INTEGER NOT NULL,
+  description TEXT NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  post_code VARCHAR(255) NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  date_created TIMESTAMP DEFAULT NOW()
+);
+
+-- Drop and recreate favorites table
+
+DROP TABLE IF EXISTS favorites CASCADE;
+CREATE TABLE favorites (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  posting_id INTEGER REFERENCES postings(id) ON DELETE CASCADE
+);
+
+-- Drop and recreate conversations table
+
+DELETE TABLE IF EXISTS conversations CASCADE;
+CREATE TABLE conversations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  seller_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  buyer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  posting_id INTEGER REFERENCES postings(id) ON DELETE CASCADE
+);
+
+-- Drop and recreate messages table
+
+DROP TABLE IF EXISTS messages CASCADE;
+CREATE TABLE messages (
+  id SERIAL PRIMARY KEY NOT NULL,
+  sender_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  receiver_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  content TEXT,
+  conversation_id INTEGER REFERENCES conversations(id) ON DELETE CASCADE,
+  time_sent TIMESTAMP DEFAULT NOW()
+);
