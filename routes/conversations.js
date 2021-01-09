7a63c29b669b06a285eca5ed17ee21f8ec104046
luke-berzins <lukeberzins16@gmail.com>
2021-01-09 19:27:20 +0000

    /conversations now displays conversations

--- routes/conversations.js
+++ routes/conversations.js
@@ -12,8 +12,8 @@ module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM conversations;`)
       .then(data => {
-        const users = data.rows;
-        res.json({ users });
+        const templateVars = { conversations: data.rows};
+        res.render('conversations', templateVars)
       })
       .catch(err => {
         res
