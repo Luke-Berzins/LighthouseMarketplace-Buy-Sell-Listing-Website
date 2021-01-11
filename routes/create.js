3c83935f5783a6def3cc00bd0a662e0e63df558b
Robert <rbbenett0614@gmail.com>
2021-01-11 23:46:27 +0000

    Moved router.post into module exports in create.js

--- routes/create.js
+++ routes/create.js
@@ -8,21 +8,22 @@ module.exports = (db) => {
       };
       res.render("create", templateVars);
   });
+  router.post("/create", (req, res) => {
+    const userId = req.session["userID"];
+    const queryString = `
+    INSERT INTO postings (image_photo_url, title, price, description, street, city, province, post_code, country)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+    RETURNING *;`;
+    const values = [image, title, price, description, street, city, province, postal_code, country];
+      return db.query(queryString, values)
+      .then(res => {
+        res.redirect("/");
+        return res.rows[0];
+      })
+      .catch(err => {
+        return console.log('query error:', err);
+      })
+    });
   return router;
 };
-router.post("/create", (req, res) => {
-  const userId = req.session["userID"];
-  const queryString = `
-  INSERT INTO postings (image_photo_url, title, price, description, street, city, province, post_code, country)
-  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-  RETURNING *;`;
-  const values = [image, title, price, description, street, city, province, postal_code, country];
-    return db.query(queryString, values)
-    .then(res => {
-      res.redirect("/");
-      return res.rows[0];
-    })
-    .catch(err => {
-      return console.log('query error:', err);
-    })
-  });
+
