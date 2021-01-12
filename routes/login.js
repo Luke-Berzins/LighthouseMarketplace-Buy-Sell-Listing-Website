62cdc927a26479e0ce5847ac7f535aacf65d8223
saadkureshi <saq619@gmail.com>
2021-01-12 02:12:22 +0000

    admin navbar updated

--- routes/login.js
+++ routes/login.js
@@ -18,18 +18,21 @@ module.exports = (db) => {
     let email = req.body.email;
     let password = req.body.password;
       return db.query(`
-      SELECT id, name, email, password
+      SELECT id, name, email, password, is_admin
       FROM users
       WHERE email = $1
     `, [email])
     .then(response => {
       if (response.rows[0]) {
+        console.log("RESPONSE IS >>>>>", response.rows[0]);
         if (bcrypt.compareSync(password, response.rows[0].password)) {
           console.log("user match in database");
           let userName = response.rows[0].name;
           let userID = response.rows[0].id;
+          let isAdmin = response.rows[0].is_admin;
           req.session["userName"] = userName;
           req.session["userID"] = userID;
+          req.session["isAdmin"] = isAdmin
           res.redirect("/postings");
         }
         else {
