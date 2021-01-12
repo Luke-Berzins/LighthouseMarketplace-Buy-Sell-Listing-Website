278b043600cce404ba5b125151c72186ce782b94
Robert <rbbenett0614@gmail.com>
2021-01-12 00:10:05 +0000

    Updated post route for create new listing so it posts to the database

--- routes/create.js
+++ routes/create.js
@@ -8,16 +8,26 @@ module.exports = (db) => {
       };
       res.render("create", templateVars);
   });
-  router.post("/create", (req, res) => {
-    const userId = req.session["userID"];
+
+  router.post("/", (req, res) => {
+    const userName = req.session["userName"];
+    let image = req.body.image;
+    let title = req.body.title;
+    let price = req.body.price;
+    let description = req.body.description;
+    let street = req.body.street;
+    let city = req.body.city;
+    let province = req.body.province;
+    let post_code = req.body.post_code;
+    let country = req.body.country;
     const queryString = `
     INSERT INTO postings (image_photo_url, title, price, description, street, city, province, post_code, country)
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
     RETURNING *;`;
-    const values = [image, title, price, description, street, city, province, postal_code, country];
+    const values = [image, title, price, description, street, city, province, post_code, country];
       return db.query(queryString, values)
-      .then(res => {
-        res.redirect("/");
+      .then(response => {
+        res.redirect("/postingss");
         return res.rows[0];
       })
       .catch(err => {
