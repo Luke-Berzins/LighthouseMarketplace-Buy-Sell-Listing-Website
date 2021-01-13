d52630c8d4fb016ae6dfa9775a7df45383d58052
luke-berzins <lukeberzins16@gmail.com>
2021-01-13 02:54:42 +0000

    adding messages to the database

--- public/scripts/conversations_app.js
+++ public/scripts/conversations_app.js
@@ -2,7 +2,11 @@ $(() => {
   $(".conversation_title").click(function(){
      $(this).siblings(".conversations-container").toggle();
   });
+
+  let conversationID = $(this).siblings(".conversation_id");
+
 });
+
 
 
 
