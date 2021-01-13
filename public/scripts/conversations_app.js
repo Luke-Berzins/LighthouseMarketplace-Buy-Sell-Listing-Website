d28043eb1131a8469775771fff0e2353d523e01b
saadkureshi <saq619@gmail.com>
2021-01-13 21:18:24 +0000

    Update

--- public/scripts/conversations_app.js
+++ public/scripts/conversations_app.js
@@ -6,20 +6,3 @@ $(() => {
   let conversationID = $(this).siblings(".conversation_id");
 
 });
-
-
-
-
-
-// $(() => {
-//   $(".far").on('click', function(e) {
-//     e.preventDefault();
-//     $.ajax({
-//       method: "POST",
-//       url: "/postings",
-//       data: {postingId: 5}
-//     }).then((response) => {
-//       console.log(response);
-//     });;
-//   });
-//   });
