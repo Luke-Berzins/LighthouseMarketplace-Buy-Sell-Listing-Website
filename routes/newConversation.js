dede2784bf0ab1599ba37ce3e416e622885acc31
Robert <rbbenett0614@gmail.com>
2021-01-14 16:27:29 +0000

    Fixed new postings

--- routes/newConversation.js
+++ routes/newConversation.js
@@ -29,7 +29,7 @@ module.exports = (db) => {
           ($1, $2, $3, $4, Now())
           ;`, [sender_id, Number(receiver_id), newMessage, response.rows[0]["id"]])
         })
-        .then(res => {
+        .then(resp => {
           res.redirect("/conversations")
         })
         .catch(e => res.send(e));
