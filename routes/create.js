79348412ad8874621b958e3bb0be26168d14b7ab
saadkureshi <saq619@gmail.com>
2021-01-14 17:19:34 +0000

    linted

--- routes/create.js
+++ routes/create.js
@@ -3,11 +3,11 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-      const templateVars = {
-        user: req.session["userName"],
-        isAdmin: req.session["isAdmin"]
-      };
-      res.render("create", templateVars);
+    const templateVars = {
+      user: req.session["userName"],
+      isAdmin: req.session["isAdmin"]
+    };
+    res.render("create", templateVars);
   });
 
   router.post("/", (req, res) => {
@@ -27,15 +27,15 @@ module.exports = (db) => {
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
     RETURNING *;`;
     const values = [user_id, image, title, price, description, location_name, street, city, province, post_code, country];
-      return db.query(queryString, values)
+    return db.query(queryString, values)
       .then(response => {
         res.redirect("/postings");
         return res.rows[0];
       })
       .catch(err => {
         return console.log('query error:', err);
-      })
-    });
+      });
+  });
   return router;
 };
 
--- routes/newConversation.js
+++ routes/newConversation.js
@@ -17,22 +17,22 @@ module.exports = (db) => {
           ($1, $2, $3)
           RETURNING *
           ;`, [Number(receiver_id), Number(sender_id), Number(posting_id)])
-        .then(response => {
-          console.log("000000", response.rows)
-          console.log("000000", req.body)
-          let sender_id = req.session["userID"];
-          let newMessage = req.body["newMessage"];
-          let receiver_id = req.body["receiver_id"];
-          return db.query(`
+      .then(response => {
+        console.log("000000", response.rows);
+        console.log("000000", req.body);
+        let sender_id = req.session["userID"];
+        let newMessage = req.body["newMessage"];
+        let receiver_id = req.body["receiver_id"];
+        return db.query(`
           INSERT INTO messages (sender_id, receiver_id, content, conversation_id, time_sent)
           VALUES
           ($1, $2, $3, $4, Now())
-          ;`, [sender_id, Number(receiver_id), newMessage, response.rows[0]["id"]])
-        })
-        .then(res => {
-          res.redirect("/conversations")
-        })
-        .catch(e => res.send(e));
+          ;`, [sender_id, Number(receiver_id), newMessage, response.rows[0]["id"]]);
+      })
+      .then(res => {
+        res.redirect("/conversations");
+      })
+      .catch(e => res.send(e));
 
 
   });
