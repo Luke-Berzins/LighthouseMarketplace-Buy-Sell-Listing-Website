a1ff0b413c0f895c47c1d603740eec135ea0c1d6
luke-berzins <lukeberzins16@gmail.com>
2021-01-14 03:08:57 +0000

    postings now creates a new conversation

--- routes/newConversation.js
+++ routes/newConversation.js
@@ -7,7 +7,33 @@ module.exports = (db) => {
   });
 
   router.post("/", (req, res) => {
-    console.log("Hellllllooooooooo")
+    console.log("heeeeere", req.body);
+    let sender_id = req.session["userID"];
+    let receiver_id = req.body["receiver_id"];
+    let posting_id = req.body["posting_id"];
+    return db.query(`
+          INSERT INTO conversations (seller_id, buyer_id, posting_id)
+          VALUES
+          ($1, $2, $3)
+          RETURNING *
+          ;`, [Number(receiver_id), Number(sender_id), Number(posting_id)])
+        .then(response => {
+          console.log("000000", response.rows)
+          console.log("000000", req.body)
+          let sender_id = req.session["userID"];
+          let newMessage = req.body["newMessage"];
+          let receiver_id = req.body["receiver_id"];
+          return db.query(`
+          INSERT INTO messages (sender_id, receiver_id, content, conversation_id, time_sent)
+          VALUES
+          ($1, $2, $3, $4, Now())
+          ;`, [sender_id, Number(receiver_id), newMessage, response.rows[0]["id"]])
+        })
+        .then(resp => {
+          res.redirect("/conversations")
+        })
+        .catch(e => res.send(e));
+
 
   });
   return router;
