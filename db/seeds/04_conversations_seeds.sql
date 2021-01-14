f84563b0b9648fb404f1613c21a7ded8191dfa8c
luke-berzins <lukeberzins16@gmail.com>
2021-01-14 20:14:05 +0000

    making seeds consitent with website structure

--- db/seeds/04_conversations_seeds.sql
+++ db/seeds/04_conversations_seeds.sql
@@ -1,10 +1,13 @@
-INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (5, 10, 4);
-INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (6, 4, 7);
-INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (2, 8, 3);
-INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (1, 9, 1);
-INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (3, 7, 2);
-INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (3, 6, 2);
-INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (4, 9, 5);
-INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (6, 2, 12);
-INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (1, 6, 3);
+INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (8, 4, 1);
+INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (8, 5, 5);
+INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (8, 6, 10);
+INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (8, 7, 1);
+INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (9, 7, 2);
+INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (9, 6, 6);
+INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (9, 1, 7);
+INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (9, 4, 12);
+INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (10, 1, 3);
+INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (10, 2, 4);
+INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (10, 1, 8);
+INSERT INTO conversations (seller_id, buyer_id, posting_id) VALUES (10, 3, 11);
 
