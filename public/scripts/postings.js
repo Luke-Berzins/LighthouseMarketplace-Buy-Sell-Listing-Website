10e4e8896631194e92a98b5caed2ce57a0eec237
saadkureshi <saq619@gmail.com>
2021-01-14 22:13:50 +0000

    Delete button added

--- public/scripts/postings.js
+++ public/scripts/postings.js
@@ -17,13 +17,12 @@ $(() => {
 });
 
 $(() => {
-  $(".mark-item-button").on('click', function(e) {
+  $(".mark-item-button, .remove-item-button").on('click', function(e) {
     e.preventDefault();
     $(this).siblings("#item-availability").toggleClass("fa-check fa-times-circle");
     let buttonText = $(this).text();
     $(this).text(buttonText == "Mark as Sold" ? "Mark as Available" : "Mark as Sold");
     const $postingIdClicked = $(this).parents(".posting-container").find(".posting-id").text();
-
     $.ajax({
       method: "POST",
       url: "/markItems",
@@ -34,6 +33,14 @@ $(() => {
     }).then((res) => {
       console.log("AJAX request completed.");
     });
+  });
+});
+
+$(() => {
+  $(".remove-item-button").on('click', function(e) {
+    e.preventDefault();
+    const $postingContainer = $(this).parents(".posting-container");
+    $postingContainer.remove();
   });
 });
 
